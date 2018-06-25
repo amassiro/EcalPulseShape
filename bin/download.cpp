fa3b4ef4ef3263c3a37cb841288e80cef548c279
amassiro <massironi.andrea@gmail.com>
2018-06-25 17:40:19 +0200

    steps forward

--- bin/download.cpp
+++ bin/download.cpp
@@ -26,12 +26,18 @@ int main(int argc, char** argv) {
  std::cout << "Download pulse shapes " << std::endl;
 
  
- cond::CondDBDumper<EcalPulseShapes> PulseShapeGeneration("EcalPulseShapes_hlt");
- PulseShapeGeneration.run(argc, argv);
+//  cond::CondDBDumper<EcalPulseShapes> PulseShapeGeneration("EcalPulseShapes_hlt");
+//  PulseShapeGeneration.run(argc, argv);
  
  
- cond::CondDBDumper<EcalPulseShapes> PulseShapeFit("EcalPulseShapes_hlt");
- PulseShapeFit.run(argc, argv);
+//  cond::CondDBDumper<EcalPulseShapes> PulseShapeFit("EcalPulseShapes_hlt");
+//  PulseShapeFit.run(argc, argv);
+ 
+ 
+ cond::CondDBDumper<EcalPulseShapes> PulseShapeGenerationAndFit("EcalPulseShapes_hlt", "EcalPulseShapes_hlt");
+ PulseShapeGenerationAndFit.run(argc, argv);
+ 
+ 
  
  //---- generate according to PulseShapeGeneration and fit using PulseShapeFit
  
--- interface/CondDBDumper.h
+++ interface/CondDBDumper.h
@@ -40,6 +40,16 @@
 #include "DataFormats/EcalDetId/interface/EBDetId.h"
 #include "DataFormats/EcalDetId/interface/EEDetId.h"
 
+//---- for multifit 
+#include "RecoLocalCalo/EcalRecAlgos/interface/PulseChiSqSNNLS.h"
+
+#include "CondFormats/DataRecord/interface/EcalPulseShapesRcd.h"
+#include "CondFormats/DataRecord/interface/EcalPulseCovariancesRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalPulseShapes.h"
+#include "CondFormats/EcalObjects/interface/EcalPulseCovariances.h"
+
+
 namespace cond {
   
   template<class C>
@@ -48,37 +58,17 @@ namespace cond {
       
   public:
     
-//     CondDBDumper(std::string tag) : Utilities("conddb_dumper"), _tag(tag)
-    CondDBDumper(std::string tag)  : Utilities("")
-//       CondDBDumper(std::string tag)  
-        {
-     _tag = tag;
-     
-//       addAuthenticationOptions();
-//       addOption<bool>("join", "j", "produce one single output file, where IOVs are separated by double line break and a comment line starting with `#'");
-//       addOption<bool>("verbose","v","verbose");
-//       addOption<cond::Time_t>("beginTime","b","begin time (first since) (optional)");
-//       addOption<cond::Time_t>("endTime","e","end time (last till) (optional)");
-//       addOption<int>("niov","n","number of IOVs to be dumped");
-//       addOption<int>("prescale","s","prescale factor, i.e. dump 1 in N IOVs");
-//       addOption<std::string>("object","O","object to be dumped (required)");
-//       addOption<std::string>("output","o","output file");
-//       addOption<std::string>("tag","t","tag to be dumped (required)");
-//       addOption<std::string>("db","d","database to run the command on;"
-//       " arg can be an explicit db connection, e.g. for sqlite files,"
-//       " or one of the following aliases:\n"
-//       "     dev -> FrontierPrep\n"
-//       "     pro -> PromptProd\n"
-//       "     arc -> FrontierArc\n"
-//       "     int -> FrontierInt\n"
-//       );
+    CondDBDumper(std::string tag_production, std::string tag_fit)  : Utilities("") {
+      _tag_production = tag_production;
+      _tag_fit        = tag_fit;
       
-     
+      _bias.resize(EBDetId::MAX_HASH + 1 + EEDetId::kSizeForDenseIndexing);
       _ids.resize(EBDetId::MAX_HASH + 1 + EEDetId::kSizeForDenseIndexing);      
       for (int hi = EBDetId::MIN_HASH; hi <= EBDetId::MAX_HASH; ++hi ) {
         EBDetId ebId = EBDetId::unhashIndex(hi);
         if (ebId != EBDetId()) {
           _ids[hi] = ebId;
+          _bias[hi] = 1.;
         }
       }
       for ( int hi = 0; hi < EEDetId::kSizeForDenseIndexing; ++hi ) {
@@ -86,6 +76,7 @@ namespace cond {
         if (eeId != EEDetId()) {
           int idx = EBDetId::MAX_HASH + 1 + hi;
           _ids[idx] = eeId;
+          _bias[idx] = -1.;
         }
       }
       assert(_ids.size() == 75848);
@@ -124,43 +115,142 @@ namespace cond {
       cond::persistency::Session session = connPool.createSession(connect);
 
 //       std::string tag = "EcalPulseShapes_hlt";
-      std::string tag = _tag;
+//       std::string tag = _tag_production;
       
       
       
       session.transaction().start( true );
-      cond::persistency::IOVProxy iov = session.readIov(tag, true);
-      std::string obj_type = iov.payloadObjectType();
+      
+      cond::persistency::IOVProxy iovs_EcalPulseCovariances = session.readIov("EcalPulseCovariances_mc", true);
+//       EcalPulseCovariancesRcd       EcalPulseCovariances_mc 
+      
+      std::shared_ptr<EcalPulseCovariances> pulsecovariances;
+      for (auto iov : iovs_EcalPulseCovariances) {
+        pulsecovariances = session.fetchPayload<EcalPulseCovariances>(iov.payloadId);
+      }
       
       
-      auto first_iov = iov.begin();
-      auto last_iov  = iov.begin();
-      for (int i = 0; i < iov.loadedSize() - 1; ++i) ++last_iov;
+      cond::persistency::IOVProxy iovs_production = session.readIov(_tag_production, true);
+      std::string obj_type = iovs_production.payloadObjectType();
+      
+      
+      
+      auto first_iov = iovs_production.begin();
+      auto last_iov  = iovs_production.begin();
+      for (int i = 0; i < iovs_production.loadedSize() - 1; ++i) ++last_iov;
 
       
       std::cout << (*first_iov).since << " " << (*last_iov).since << std::endl;
       
       int cnt_iov = 0;
       //---- loop over all the intervals of validity
-      for (auto i : iov) {
+      for (auto iov : iovs_production) {
         cnt_iov++;
-        std::shared_ptr<C> pa = session.fetchPayload<C>(i.payloadId);
+        std::shared_ptr<C> pa = session.fetchPayload<C>(iov.payloadId);
         //---- loop over the crystals
         for (size_t i = 0; i < _ids.size(); ++i) {
           DetId id(_ids[i]);
-          EcalPulseShapes::const_iterator it = pa->find(id);
-          if (it == pa->end()) {
+          EcalPulseShapes::const_iterator it_pulseShape = pa->find(id);
+          
+          if (it_pulseShape == pa->end()) {
             std::cout << "Cannot find value for DetId " << id.rawId() << std::endl;
           }
+          
           coord(_ids[i]);
-          std::cout <<  _c.ix_ << " " <<  _c.iy_ << " " <<  _c.iz_ << " ::: " ;
-          //---- loop over the N-samples of the pulse shape
-          for (int is = 0; is < EcalPulseShape::TEMPLATESAMPLES; ++is) {
-            std::cout << it->val(is) << " : " ;
+          
+//           std::vector < float > production_samples;
+//           production_samples.push_back(0, 0, 0);     
+
+          SampleVector amplitudes;
+          amplitudes[0] = 0.;
+          amplitudes[1] = 0.;
+          amplitudes[2] = 0.;
+
+          for (int iSample = 0; iSample < EcalPulseShape::TEMPLATESAMPLES; iSample++) {
+            //                       std::cout << it_pulseShape->val(iSample) << " : " ;
+            //---- only the first 7 samples, the rest is extrapolation
+            if (iSample<7) {
+              //---- 100 = 100 ADC counts [random number]
+              //               production_samples.push_back( 100* it_pulseShape->val(iSample) );
+              amplitudes[iSample+3] = 100* it_pulseShape->val(iSample);
+            }
           }
-          std::cout << std::endl;
+          
+                    
+          SampleMatrix noisecov;
+          BXVector activeBX;
+          FullSampleVector fullpulse(FullSampleVector::Zero());
+          FullSampleMatrix fullpulsecov(FullSampleMatrix::Zero());
+
+          const EcalPulseCovariances::Item * aPulseCov = nullptr;
+          unsigned int hashedIndex = EBDetId(_ids[i]).hashedIndex();
+          aPulseCov  = &(pulsecovariances)->barrel(hashedIndex);
+          
+          
+          SampleGainVector gainsPedestal;
+          SampleGainVector badSamples = SampleGainVector::Zero();
+          
+
+          
+          for (int iSample=0; i<EcalPulseShape::TEMPLATESAMPLES; ++iSample) {
+            fullpulse(iSample+7) = it_pulseShape->val(iSample);
+          }
+          
+          for(int iSample=0; iSample<EcalPulseShape::TEMPLATESAMPLES;iSample++) {
+            for(int jSample=0; jSample<EcalPulseShape::TEMPLATESAMPLES;jSample++) {
+              fullpulsecov(iSample+7,jSample+7) = aPulseCov->covval[iSample][jSample];
+            }
+          }
+            
+            
+            
+//           noisecov = aped->rms_x12*aped->rms_x12*noisecors[0];
+          //---- 1.2 ADC: https://github.com/kpedro88/cmssw/blob/master/CondFormats/EcalObjects/interface/EcalPedestals.h ---> it's a float
+          SampleMatrixGainArray noisecors;
+          noisecov = 1.2 * 1.2 * noisecors[0];
+          
+          for (int iSample = 0; iSample < EcalPulseShape::TEMPLATESAMPLES; iSample++) {
+            gainsPedestal[iSample] = -1;  //-1 for static pedestal
+          }
+          
+          bool status = _pulsefunc.DoFit(amplitudes,noisecov,activeBX,fullpulse,fullpulsecov,gainsPedestal,badSamples);
+          float chisq = _pulsefunc.ChiSq();
+          
+//           if (!status) {
+//             edm::LogWarning("EcalUncalibRecHitMultiFitAlgo::makeRecHit") << "Failed Fit" << std::endl;
+//           }
+//           
+          unsigned int ipulseintime = 0;
+          for (unsigned int ipulse=0; ipulse<_pulsefunc.BXs().rows(); ++ipulse) {
+            if (_pulsefunc.BXs().coeff(ipulse)==0) {
+              ipulseintime = ipulse;
+              break;
+            }
+          }
+          
+          _bias [i] = ( status ? _pulsefunc.X()[ipulseintime] : 0.) / 100.;
+          
+//           
+//           amplitude = status ? _pulsefunc.X()[ipulseintime] : 0.;
+//           amperr = status ? _pulsefunc.Errors()[ipulseintime] : 0.;
+//           
+//           
+          
+          
+          
+          
+          
+          
+//           std::cout <<  _c.ix_ << " " <<  _c.iy_ << " " <<  _c.iz_ << " ::: " ;
+//           //---- loop over the N-samples of the pulse shape
+//           for (int is = 0; is < EcalPulseShape::TEMPLATESAMPLES; ++is) {
+          //             std::cout << it_pulseShape->val(is) << " : " ;
+//           }
+//           std::cout << std::endl;
         }
       }
+      
+      std::cout << " cnt_iov = " << cnt_iov << std::endl;
       
       
       session.transaction().commit();
@@ -207,8 +297,14 @@ namespace cond {
     
     
   private:
-    std::string _tag;
+    std::string _tag_production;
+    std::string _tag_fit;
     std::vector<DetId> _ids;
+    std::vector<float> _bias;
+    
+    PulseChiSqSNNLS _pulsefunc;
+    
+    
   };
   
 }
